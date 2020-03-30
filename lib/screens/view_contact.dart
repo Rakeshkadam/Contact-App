b05accc8b79dc6924fe6ac5e8b0880d573284c89
Rakesh Kadam <rakesh22kadam@outlook.com>
2020-03-30 20:44:39 +0530

    Added delete contact feature

--- lib/screens/view_contact.dart
+++ lib/screens/view_contact.dart
@@ -53,11 +53,206 @@ class _ViewContactState extends State<ViewContact> {
       throw 'Could not send sms $number';
     }
   }
+
+  deleteContact(){
+    showDialog(
+      context: context,
+      builder: (BuildContext context){
+        return AlertDialog(
+          title: Text("Delete ?"),
+          content: Text("Delete Contact ?"),
+          actions: <Widget>[
+            FlatButton(
+              onPressed: (){
+                Navigator.of(context).pop();
+              }, 
+              child: Text("Cancel"),
+            ),
+            FlatButton(
+              onPressed: () async{
+                Navigator.of(context).pop();
+                await _databaseReference.child(id).remove();
+                navigateToLastScreen();
+              },
+              child: Text("Delete"),
+              ),
+          ],
+        );
+      }
+    );
+  }
+
+  navigateToEditScreen(id){
+    Navigator.push(context, MaterialPageRoute(
+      builder:(context){
+        //Todo: Add Id
+        return EditContact();
+      } ));
+  }
+
+  navigateToLastScreen(){
+    Navigator.pop(context);
+  }
   
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    // wrap screen in WillPopScreen widget
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("View Contact"),
+      ),
+      body: Container(
+        child: isLoading
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : ListView(
+                children: <Widget>[
+                  // header text container
+                  Container(
+                      height: 200.0,
+                      child: Image(
+                        //
+                        image: _contact.photoUrl == "empty"
+                            ? AssetImage("assets/logo.png")
+                            : NetworkImage(_contact.photoUrl),
+                        fit: BoxFit.contain,
+                      )),
+                  //name
+                  Card(
+                    elevation: 2.0,
+                    child: Container(
+                        margin: EdgeInsets.all(20.0),
+                        width: double.maxFinite,
+                        child: Row(
+                          children: <Widget>[
+                            Icon(Icons.perm_identity),
+                            Container(
+                              width: 10.0,
+                            ),
+                            Text(
+                              "${_contact.firstName} ${_contact.lastName}",
+                              style: TextStyle(fontSize: 20.0),
+                            ),
+                          ],
+                        )),
+                  ),
+                  // phone
+                  Card(
+                    elevation: 2.0,
+                    child: Container(
+                        margin: EdgeInsets.all(20.0),
+                        width: double.maxFinite,
+                        child: Row(
+                          children: <Widget>[
+                            Icon(Icons.phone),
+                            Container(
+                              width: 10.0,
+                            ),
+                            Text(
+                              _contact.phone,
+                              style: TextStyle(fontSize: 20.0),
+                            ),
+                          ],
+                        )),
+                  ),
+                  // email
+                  Card(
+                    elevation: 2.0,
+                    child: Container(
+                        margin: EdgeInsets.all(20.0),
+                        width: double.maxFinite,
+                        child: Row(
+                          children: <Widget>[
+                            Icon(Icons.email),
+                            Container(
+                              width: 10.0,
+                            ),
+                            Text(
+                              _contact.email,
+                              style: TextStyle(fontSize: 20.0),
+                            ),
+                          ],
+                        )),
+                  ),
+                  // address
+                  Card(
+                    elevation: 2.0,
+                    child: Container(
+                        margin: EdgeInsets.all(20.0),
+                        width: double.maxFinite,
+                        child: Row(
+                          children: <Widget>[
+                            Icon(Icons.home),
+                            Container(
+                              width: 10.0,
+                            ),
+                            Text(
+                              _contact.address,
+                              style: TextStyle(fontSize: 20.0),
+                            ),
+                          ],
+                        )),
+                  ),
+                  // call and sms
+                  Card(
+                    elevation: 2.0,
+                    child: Container(
+                        margin: EdgeInsets.all(20.0),
+                        width: double.maxFinite,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: <Widget>[
+                            IconButton(
+                              iconSize: 30.0,
+                              icon: Icon(Icons.phone),
+                              color: Colors.red,
+                              onPressed: () {
+                                callAction(_contact.phone);
+                              },
+                            ),
+                            IconButton(
+                              iconSize: 30.0,
+                              icon: Icon(Icons.message),
+                              color: Colors.red,
+                              onPressed: () {
+                                smsAction(_contact.phone);
+                              },
+                            )
+                          ],
+                        )),
+                  ),
+                  // edit and delete
+                  Card(
+                    elevation: 2.0,
+                    child: Container(
+                        margin: EdgeInsets.all(20.0),
+                        width: double.maxFinite,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: <Widget>[
+                            IconButton(
+                              iconSize: 30.0,
+                              icon: Icon(Icons.edit),
+                              color: Colors.red,
+                              onPressed: () {
+                                navigateToEditScreen(id);
+                              },
+                            ),
+                            IconButton(
+                              iconSize: 30.0,
+                              icon: Icon(Icons.delete),
+                              color: Colors.red,
+                              onPressed: () {
+                                deleteContact();
+                              },
+                            )
+                          ],
+                        )),
+                  )
+                ],
+              ),
+      ),
     );
   }
 }
